74e7978995203fd6ede20641642b7ede9a07ad73
Monika Biestek <Monika Biestek>
2020-05-17 11:51:11 +0200

    hashing added

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -1,16 +1,23 @@
 package com.company.enroller.persistence;
 
-import com.company.enroller.model.Participant;
+import java.util.Collection;
+
 import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import java.util.Collection;
+import com.company.enroller.model.Participant;
 
 @Component("participantService")
 public class ParticipantService {
 
     DatabaseConnector connector;
-
+    
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
+    
     public ParticipantService() {
         connector = DatabaseConnector.getInstance();
     }
@@ -24,6 +31,9 @@ public class ParticipantService {
     }
 
     public Participant add(Participant participant) {
+    	String rawPassword=participant.getPassword();
+    	String hashedPassword=this.passwordEncoder.encode(rawPassword);
+    	participant.setPassword(hashedPassword);
         Transaction transaction = connector.getSession().beginTransaction();
         connector.getSession().save(participant);
         transaction.commit();
